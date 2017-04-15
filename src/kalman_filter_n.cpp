3227adc0cccb6fccc3554f13b22e8d0a1508e57d
nishanthjois <nishanth.jois@gmail.com>
2017-04-16 00:33:06 +0530

    Add files via upload

---
+++ src/kalman_filter_n.cpp
@@ -0,0 +1,90 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+
+  	x_=x_*F_;
+ 	 MatrixXd Ft = F_.transpose();
+ 	 P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+
+  	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+ 	 MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd K = PHt *  S.inverse();
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
+
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+
+  double rho = sqrt((x_[0]*x_[0])*(x_[1]*x_[1]));
+  double phi;
+  double rho_dot;
+
+  if (x_[0]!=0){
+    phi = atan2(x_[1],x_[0]);
+    rho_dot = ((x_[0] * x_[2] + x_[1] * x_[3]) / rho);
+  } else {
+    phi= 0;
+    rho_dot = 0;
+  }
+
+MatrixXd z_pred (3,1);
+z_pred << rho,phi,rho_dot;
+
+  //VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();       // this is Hj -jacobian
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd K = PHt * S.inverse();              
+
+  // Update the estimate and new covariance
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I- K * H_) * P_;
+
+
+  
+
+}
